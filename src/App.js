8c36e63e1ed601509df165181564121f9431f0b8
Ruhulcse <ruhul.cse7862@gmail.com>
2021-03-24 18:06:54 +0600

    work section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./Components/About";
 import Head from "./Components/Head";
 import Service from "./Components/Service";
+import Work from "./Components/Work";
 import { LanguageContextProvider } from "./context/language";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Head />
       <About />
       <Service />
+      <Work />
     </LanguageContextProvider>
   );
 }
---
+++ src/Components/Work.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Work() {
+    return (
+        <div className="container-fluid">
+            <div className="row pt-3"></div>
+            <div className="d-flex justify-content-center pt-5">
+                <h1>How We Work</h1>
+            </div>
+            <div className="d-flex justify-content-center">
+               <p>simple process at which you can make your ideas possible with 3 easy steps</p>
+            </div>
+            <div className="row">
+                <div className="col-md-4 pl-5">
+                    <h1>hello</h1>
+                </div>
+                <div className="col-md-8">
+                    <h1>hello</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Work
