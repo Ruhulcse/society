a040e1cbf196f5d04d17343afa64070ca645428b
readwanmd <readwanmd@gmail.com>
2021-03-25 12:19:49 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import "./App.css";
-import About from "./Components/About";
-import Head from "./Components/Head";
-import Service from "./Components/Service";
-import Work from "./Components/Work";
-import { LanguageContextProvider } from "./context/language";
+import './App.css';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Head from './Components/Head';
+import Service from './Components/Service';
+import Work from './Components/Work';
+import { LanguageContextProvider } from './context/language';
 
 function App() {
-  return (
-    <LanguageContextProvider>
-      <Head />
-      <About />
-      <Service />
-      <Work />
-    </LanguageContextProvider>
-  );
+	return (
+		<LanguageContextProvider>
+			<Head />
+			<About />
+			<Service />
+			<Work />
+			<Footer />
+		</LanguageContextProvider>
+	);
 }
 
 export default App;
