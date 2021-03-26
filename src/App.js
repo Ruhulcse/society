14fb7b40be23a0baf15d15b62b57e383fed34755
ruhul <ruhul.cse7862@gmail.com>
2021-03-26 09:51:29 +0600

    add contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./Components/About";
 import Footer from "./Components/Footer";
 import Head from "./Components/Head";
 import Service from "./Components/Service";
+import Contact from "./Components/ContactUs"
 import Work from "./Components/Work";
 import { LanguageContextProvider } from "./context/language";
 
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Service />
       <Work />
+	  <Contact/>
       <Footer />
     </LanguageContextProvider>
   );
