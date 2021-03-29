60d981228cefd8739bcc60a72cb7c5d75569c2d6
readwanmd <readwanmd@gmail.com>
2021-03-29 13:22:20 +0600

    news section done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import "./App.css";
-import About from "./Components/About";
-import Footer from "./Components/Footer";
-import Head from "./Components/Head";
-import Service from "./Components/Service";
-import Work from "./Components/Work";
-import { LanguageContextProvider } from "./context/language";
+import './App.css';
+import About from './Components/About';
+import ContactUs from './Components/ContactUs';
+import Footer from './Components/Footer';
+import Head from './Components/Head';
+import LatestNews from './Components/LatestNews';
+import Service from './Components/Service';
+import Work from './Components/Work';
+import { LanguageContextProvider } from './context/language';
 
 function App() {
-  return (
-    <LanguageContextProvider>
-      <Head />
-      <About />
-      <Service />
-      <Work />
-      <Footer />
-    </LanguageContextProvider>
-  );
+	return (
+		<LanguageContextProvider>
+			<Head />
+			<About />
+			<Service />
+			<Work />
+			<LatestNews />
+			<ContactUs />
+			<Footer />
+		</LanguageContextProvider>
+	);
 }
 
 export default App;
