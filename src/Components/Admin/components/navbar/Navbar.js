f6ba8913ca6ae96d1a07881d7133183bee096504
Ruhulcse <ruhul.cse7862@gmail.com>
2021-04-12 14:08:24 +0600

    navbar section complete

--- src/Components/Admin/components/navbar/Navbar.js
+++ src/Components/Admin/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import "./Navbar.css";
-import avatar from "../../assets/avatar.svg";
+import avatar from "../../assets/profile2.jpg";
 
 const Navbar = ({ sidebarOpen, openSidebar }) => {
   return (
@@ -8,17 +8,48 @@ const Navbar = ({ sidebarOpen, openSidebar }) => {
         <i className="fa fa-bars" aria-hidden="true"></i>
       </div>
       <div className="navbar__left">
-        <a href="#">Proposal</a>
+        <h1>Proposal</h1>
       </div>
       <div className="navbar__right">
         <a href="#">
-          <i className="fa fa-search" aria-hidden="true"></i>
+          <i className="fas fa-globe mr-1" aria-hidden="true"></i>
+          <i className="fas fa-comments" aria-hidden="true"></i>
         </a>
         <a href="#">
-          <i className="fa fa-clock-o" aria-hidden="true"></i>
+          <i className="fas fa-bell" aria-hidden="true"></i>
         </a>
+        <div class="dropdown show">
+          <a
+            class="btn dropdown-toggle"
+            href="!#"
+            role="button"
+            id="dropdownMenuLink"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Luai Ahm
+          </a>
+
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item" href="!#">
+              Profile
+            </a>
+            <a class="dropdown-item" href="!#">
+              Setting
+            </a>
+            <a class="dropdown-item" href="!#">
+              Logout
+            </a>
+          </div>
+        </div>
         <a href="#!">
-          <img width="30" src={avatar} alt="avatar" />
+          <img
+            width="30"
+            style={{ borderRadius: "50%" }}
+            src={avatar}
+            alt="avatar"
+          />
         </a>
       </div>
     </nav>
