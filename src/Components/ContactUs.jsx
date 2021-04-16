f50d8e2453ca923335108d57d8c886b1eec9aae7
Ruhulcse <Ruhul.cse7862@gmail.com>
2021-04-16 15:25:01 +0600

    contact page

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,181 +1,158 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {useForm} from 'react-hook-form';
+import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faQuestionCircle, faCloudUploadAlt } from '@fortawesome/free-solid-svg-icons'
 import axios from 'axios';
+const ContactUs = () => {
+     const [name, setName] = useState("");
+     const [email, setEmail] = useState("");
+     const [phone, setPhone] = useState("");
+     const [service, setService] = useState("");
+     const [message, setMessage] = useState("");
+     const [image, setImage] = useState(null);
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(3),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+     const toBase64 = file => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
 
-function Signup() {
+     const submitHandler = async (e) =>{
+         e.preventDefault();
+         const base64Data= await toBase64(image);
+         const Proposal = {};
+         Proposal.username = name;
+         Proposal.userIdentifer = email;
+         Proposal.phoneNumber = phone;
+         Proposal.projectTitle = "Attendance System"
+         Proposal.projectType = service;
+         Proposal.description = message;
+         Proposal.attachment = base64Data;
+         try {
+            const {data}  = await axios.post("http://localhost:5000/api/v1/product/createProduct",Proposal);
+            console.log(data);
+          } catch (error) {
+            console.log(error);
+          }
+     }
+    return (
+        <div className="container">
+            <div className="bs">
+            <h1 className="mb-5">Contact Us</h1>
+            <div class="contact-form">
+					<form onSubmit={submitHandler}>
+						<div class="row">
+							<div class="input25">
+                                <h6>
+                                    Name
+                                    <span>
+                                        <abbr title="Enter ypur name">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+								<input 
+                                type="text" 
+                                placeholder="Your Name" 
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                />
+							</div>						
+							<div class="input25">
+                                <h6>
+                                    Email
+                                    <span>
+                                        <abbr title="Enter valid email">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+								<input 
+                                type="text" 
+                                placeholder="example@mail.com" 
+                                value = {email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                />
+							</div>
+							<div class="input25">
+                                <h6>
+                                    Phone Number
+                                    <span>
+                                        <abbr title="Enter valid phone number">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+								<input 
+                                type="tel" 
+                                placeholder="+123 456789" 
+                                value ={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                                />
+							</div>
+                            <div class="input25">
+                                <h6>
+                                    Service
+                                    <span>
+                                        <abbr title="Enter ypur desire Service">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+								<input 
+                                type="text"
+                                placeholder="Mobile Application"
+                                value = {service}
+                                onChange={(e) => setService(e.target.value)}
+                                 />
+							</div>
+						</div>
+						<div class="row">
+							<div class="input60">
+                                <h6>
+                                    Message
+                                    <span>
+                                        <abbr title="Enter ypur Message">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+								<textarea placeholder="Message" value={message}  onChange={(e) => setMessage(e.target.value)}></textarea>
+							</div>
+                            <div class="input30">
+                                <h6>
+                                    Idea Proposal (Optional)
+                                    <span>
+                                        <abbr title="Valid file format: JPEG / JPG / PNG">
+                                            <FontAwesomeIcon icon={faQuestionCircle} />
+                                        </abbr>
+                                    </span>
+                                </h6>
+                                <input 
+                                style={{display: 'block'}}
+                                 type="file"
+                                 name=""
+                                 id=""
+                                 onChange={(picture) => {
+                                    setImage(picture.currentTarget.files[0], false);
+                                  }}
+                                 />
+                                <FontAwesomeIcon icon={faCloudUploadAlt} />
+                                <h6>Drop file here</h6>
+                                <p>or</p>
+                                <p>Select File</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="input100">
+								<input className="bt" type="submit" value="Send" />
+							</div>
+						</div>
+					</form>
+				</div>
+            </div>
+        </div>
+    );
+};
 
-  const classes = useStyles();
-
-  const {register, handleSubmit} = useForm();
-
-  const onSubmit = async (data) => {
-     
-      const user ={};
-      user.firstName = data.name.firstName;
-      user.lastName = data.name.lastName;
-      user.userName = data.username;
-      user.email = data.email;
-      user.password = data.password;
-      user.confirmationPassword = data.confirm_password;
-
-      try {
-        const {data}  = await axios.post("http://localhost:5000/api/v1/users/signup",user);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-  }
-
-  return (
-      <div className="signin-page">
-    <Container className="signin-form" component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        
-        <Typography component="h1" variant="h4">
-          Sign up
-        </Typography>
-        <Typography className="pt-3 text-white" component="p" variant="p">
-          Please complete to sign up
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit(onSubmit)}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="name.firstName"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                inputRef={register}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="name.lastName"
-                autoComplete="lname"
-                inputRef={register}
-
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                inputRef={register}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                inputRef={register}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField  
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                inputRef={register}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField  
-                required
-                fullWidth
-                name="confirm_password"
-                label="Confirm Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                inputRef={register}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox name="t&c" value="agree" color="primary" inputRef={register} />}
-                label="I agree with terms and conditions."
-              />
-            </Grid>
-          </Grid>
-          <Grid container justify="center">
-            <Grid item>
-          <Button
-            type="submit"
-            className={`${classes.submit} create-btn`}
-          >
-            Create
-          </Button>
-            </Grid>
-          </Grid>
-          
-          <Grid container justify="center">
-            <Grid item>
-              <Link className="text-white" href="/login" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-
-          <Grid container justify="center">
-            <Grid item className="pt-2 pb-4">
-              <Link className="text-white" href="#" variant="body2">
-                <small>
-                    Terms  of use . Privacy policy
-                </small>
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-      </div>
-  );
-}
-
-export default Signup;
+export default ContactUs;
