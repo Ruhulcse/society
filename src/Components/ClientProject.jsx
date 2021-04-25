d182d7f29f512680034be85c61016db9d9a4d4f6
Ruhulcse <Ruhul.cse7862@gmail.com>
2021-04-25 21:35:24 +0600

    our project and partner project conditional rendering

--- src/Components/ClientProject.jsx
+++ src/Components/ClientProject.jsx
@@ -4,65 +4,82 @@ import project2 from "../assets/img/Screenshot -2.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CircularProgress from '@material-ui/core/CircularProgress'
 import axios from 'axios';
 
 function ClientProject() {
 
     const [projects,setProjects] = useState([]);
-  
+    const [loading, setLoading ] = useState(false);
  
     useEffect(() => {
       try {
         async function fetchprojectData() {
+          setLoading(true);
           const {data } = await axios.get("http://localhost:5000/api/v1/projects/getAllProject",);
-         
-          console.log(data)
+          let partnerProjects = data.filter(item => item.partnerproject==true);
+          console.log(partnerProjects);
           setProjects(data);
+          if(data){
+            setLoading(false);
+          }
         }
         fetchprojectData();
       } catch (error) {
         console.log(error);
       }
     }, []);
-    const data = [
-        {
-            title: "something"
-        },
-        {
-            title: "something"
-        },
-        {
-            title: "something"
-        },
-        {
-            title: "something"
-        },
-        {
-            title: "something"
-        },
-    ]
-    
-        let settings = {
-           
-            infinite: true,
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            cssEase: "linear"
-        }
+
+  
+      let settings = {
+          
+          infinite: true,
+          speed: 500,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          cssEase: "linear",
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      }
     return (
       <div className="container-fluid">
-       <Slider {...settings}>
-         {projects.map((item)=>(
-            <div className="col-md-10  pt-5 ">
-            <img src={project1}  className="img-fluid" alt="not found"/>
-            <div class="firstpic">
-            <h4 className="text-center">{item.title}</h4>
-            <a href="www.google.com"  className="firstlink">Check Project&#8811;</a>
-        </div>
-        </div>
-         ))}
-        </Slider>
+         {loading?(<CircularProgress/>):(
+            <Slider {...settings}>
+            {projects.map((item)=>(
+               <div className="col-md-10  pt-5 ">
+               <img src={project1}  className="img-fluid" alt="not found"/>
+               <div class="firstpic">
+               <h4 className="text-center">{item.title}</h4>
+               <a href="www.google.com"  className="firstlink">Check Project&#8811;</a>
+           </div>
+           </div>
+            ))}
+           </Slider>
+         )}
       </div>
     )
 }
--- src/Components/OurProject.jsx
+++ src/Components/OurProject.jsx
@@ -1,40 +1,87 @@
-import React from 'react'
-import project1 from "../assets/img/project1.png";
+import React, { useState,useEffect } from 'react';
+import project1 from "../assets/img/test.PNG";
 import project2 from "../assets/img/Screenshot -2.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CircularProgress from '@material-ui/core/CircularProgress'
+import axios from 'axios';
+
 function OurProject() {
+
+    const [projects,setProjects] = useState([]);
+    const [loading, setLoading ] = useState(false);
+ 
+    useEffect(() => {
+      try {
+        async function fetchprojectData() {
+          setLoading(true);
+          const {data } = await axios.get("http://localhost:5000/api/v1/projects/getAllProject",);
+          let partnerProjects = data.filter(item => item.partnerproject==false);
+          console.log(partnerProjects);
+          setProjects(data);
+          if(data){
+            setLoading(false);
+          }
+        }
+        fetchprojectData();
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+  
+      let settings = {
+          
+          infinite: true,
+          speed: 500,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          cssEase: "linear",
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      }
     return (
-        <div>
-            <div className="row pt-5 pb-5">
-              <div className="col-md-3 pt-5 ">
-                 <img src={project1}  className="img-fluid" alt="not found"/>
-                 <div class="firstpic">
-                    <h4 className="text-center">Troopers</h4>
-                    <a href="https://www.google.com/"  target="_blank" className="firstlink">Check Project&#8811;</a>
-                </div>
-                </div>
-                <div className="col-md-3 pt-5 ">
-                <img src={project2}  className="img-fluid" alt="not found"/>
-                <div class="firstpic">
-                    <h4 className="text-center">Skin laza</h4>
-                    <a href="https://www.google.com/" target="_blank" className="firstlink">Check Project&#8811;</a>
-                </div>
-                </div>
-                <div className="col-md-3 pt-5 ">
-                 <img src={project1}  className="img-fluid" alt="not found"/>
-                 <div class="firstpic">
-                    <h4 className="text-center">Troopers</h4>
-                    <a href="https://www.google.com/" target="_blank" className="firstlink">Check Project&#8811;</a>
-                </div>
-                </div>
-                <div className="col-md-3 pt-5 ">
-                <img src={project2}  className="img-fluid" alt="not found"/>
-                <div class="firstpic">
-                    <h4 className="text-center">Skin laza</h4>
-                    <a href="https://www.google.com/" target="_blank" className="firstlink">Check Project&#8811;</a>
-                </div>
-                </div>
-            </div>
-        </div>
+        <div className="container-fluid">
+         {loading?( <CircularProgress/>
+         ):(
+            <Slider {...settings}>
+            {projects.map((item)=>(
+               <div className="col-md-10  pt-5 ">
+               <img src={project1}  className="img-fluid" alt="not found"/>
+               <div class="firstpic">
+               <h4 className="text-center">{item.title}</h4>
+               <a href="www.google.com"  className="firstlink">Check Project&#8811;</a>
+           </div>
+           </div>
+            ))}
+           </Slider>
+         )}
+      </div>
     )
 }
 
